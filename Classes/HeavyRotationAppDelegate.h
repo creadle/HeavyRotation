7254650820e5e7c45f0adf2441d9b4a093f0448f
Chris Readle <patterner@rocketmail.com>
2010-10-04 17:19:09 -0600

    First commit for HeavyRotation

---
+++ Classes/HeavyRotationAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HeavyRotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Chris Readle (GMC-MSV-IT CONTRACTOR) on 10/4/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeavyRotationAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
